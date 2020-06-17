c4abdc8a9dbb4b9fe613f42b3218e95268e9d393
BaeJin <bjin86@gmail.com>
2020-06-17 18:32:29 +0900

    2

--- dacrawler/dacrawler/spiders/amazon.py
+++ dacrawler/dacrawler/spiders/amazon.py
@@ -55,8 +55,6 @@ class AmazonReviewSpider(scrapy.Spider):
 
     custom_settings = {
         'ITEM_PIPELINES' : {"dacrawler.pipelines.AmazonReviewPipeline": 1},
-        'FEED_FORMAT' : "csv",
-        'FEED_URI' : "amazonreview.csv",
         'USER_AGENT' : "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36"
     }
     db = Sql('salmaden')
